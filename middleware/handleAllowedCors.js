8a1f69bf19d8bad1c495e3ddbce24bebce829be7
DaoNik <nikitataranin@gmail.com>
2022-05-26 19:22:56 +0300

    feed: добавил новый роут

--- middleware/handleAllowedCors.js
+++ middleware/handleAllowedCors.js
@@ -1,12 +1,13 @@
 const allowedCors = [
-  'http://localhost:4200',
-  'http://localhost:4201',
-  'http://localhost:4202',
-  'http://www.wbschool-chat.ru',
-  'https://www.wbschool-chat.ru',
-  'http://wbschool-chat.ru',
-  'https://wbschool-chat.ru'
-]
+  "http://localhost:4200",
+  "http://localhost:4201",
+  "http://localhost:4202",
+  "http://www.wbschool-chat.ru",
+  "https://www.wbschool-chat.ru",
+  "http://wbschool-chat.ru",
+  "https://wbschool-chat.ru",
+  "https://wbbase.site",
+];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
@@ -14,7 +15,7 @@ module.exports = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
 
   const { method } = req;
@@ -22,15 +23,15 @@ module.exports = (req, res, next) => {
 
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию
   // (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers["access-control-request-headers"];
   // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
+  if (method === "OPTIONS") {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
